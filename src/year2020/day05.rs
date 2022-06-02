29da97f28fd9598c21c2317b9de1c82cd71d5c29
Sam Collins <samc3259@gmail.com>
2022-06-02 15:32:09 +0100

    polish

--- src/year2020/day05.rs
+++ src/year2020/day05.rs
@@ -1,36 +1,28 @@
-use std::collections::HashSet;
-
 pub fn part1(input: &[String]) {
-    let mut highest = 0;
-    for seat in input {
-        let id = seat_id(seat);
-        if id > highest {
-            highest = id;
-        }
-    }
+    let highest = input.iter().map(seat_id).max().unwrap();
     println!("{}", highest);
 }
 
 pub fn part2(input: &[String]) {
-    let mut seats: HashSet<i32> = (0..1 << 10).collect();
+    let mut occupied = vec![false; 1 << 10];
     for seat in input {
-        seats.remove(&seat_id(seat));
+        occupied[seat_id(seat)] = true;
     }
-    for seat in &seats {
-        if !seats.contains(&(*seat + 1)) && !seats.contains(&(*seat - 1)) {
-            println!("{}", *seat);
+    for i in 1..(1 << 10) - 1 {
+        if !occupied[i] && occupied[i - 1] && occupied[i + 1] {
+            println!("{}", i);
             break;
         }
     }
 }
 
-fn seat_id(seat: &str) -> i32 {
-    let mut total = 0;
-    for (i, letter) in seat.chars().enumerate() {
-        total += match letter {
-            'B' | 'R' => 1 << (9 - i),
-            _ => 0,
-        };
+fn seat_id(seat: &String) -> usize {
+    let mut id = 0;
+    for letter in seat.chars() {
+        id <<= 1;
+        if let 'B' | 'R' = letter {
+            id |= 1;
+        }
     }
-    total
+    id
 }
